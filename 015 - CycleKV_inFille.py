f007184b89c0dc263a6a178ffc7f159a66a324c2
Red Alert aka Max Ramanenka <mer-group@yandex.ru>
2022-02-24 10:17:49 +0300

    add 015 - CycleKV_inFille.py

---
+++ 015 - CycleKV_inFille.py
@@ -0,0 +1,23 @@
+# импорт основного окна App
+from kivy.app import App
+# GridLayout - сеточный макет
+from kivy.uix.gridlayout import GridLayout
+# импорт модуля Строитель - загружает .kv файлы в проект
+from kivy.lang import Builder
+# закрузить файл
+Builder.load_file('./015/Window.kv')
+
+# создаем основное окно программы, наследуя App - основные свойства окна
+class TestApp(App):
+    def build(self):
+        return Window()
+
+# создаем основной сеточный макет
+# описание свойств класса - ./015/Window.kv
+class Window(GridLayout):
+    pass
+
+# если программа не модуль, то выполнить
+if __name__ == '__main__':
+    # запускаем приложение
+    TestApp().run()
