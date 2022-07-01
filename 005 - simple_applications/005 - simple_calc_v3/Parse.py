c5034d68aacc1b16153dd1145387c2df360555c3
Maxim Ramanenka (Red Alert) <i@mer-group.ru>
2022-07-01 11:35:33 +0300

    add back_to_operand in Parse

--- 005 - simple_applications/005 - simple_calc_v3/Parse.py
+++ 005 - simple_applications/005 - simple_calc_v3/Parse.py
@@ -1,7 +1,34 @@
 # Parse - разбор текстовых строк
+from re import I
+
+
 class Parse:
     # если в строке есть операнд '+-*/%' 
     # то обрезать конец строки до последнего операнда
     def back_to_operand(self, line: str) -> str:
-        for operand in '+-*/%':
-            index = line.rfind(operand)
+        index = None
+        i = len(line) - 1
+        while 0 <= i:
+            if line[i] in '+-*/%':
+                index = i
+                break
+            i -= 1
+        if index is None: return line
+        else: return line[: index + 1]
+
+# тесты
+# если не модуль то выполнить программу
+if __name__ == '__main__':
+    test_1 = '1212+67676-787878+8989898'
+    test_2 = '1212+67676-787878%8989898'
+    test_3 = '1212+67676-787878-8989898'
+    test_4 = '1212+67676-787878/8989898'
+    test_5 = '1212+67676-787878*8989898'
+    test_6 = '12128989898'
+
+    print(Parse().back_to_operand(test_1))
+    print(Parse().back_to_operand(test_2))
+    print(Parse().back_to_operand(test_3))
+    print(Parse().back_to_operand(test_4))
+    print(Parse().back_to_operand(test_5))
+    print(Parse().back_to_operand(test_6))
